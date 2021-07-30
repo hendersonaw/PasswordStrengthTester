f0530f1c1c903541c0b22d4906ca013b234c306e
Aaron Henderson <HENDERSONAW@ETSU.EDU>
2021-07-30 17:42:05 -0400

    Add project files.

---
+++ PasswordStrengthTester/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rocky\source\repos\PasswordStrengthTester\PasswordStrengthTester\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
