abd453dbb378df9a750a647eabca6a064ff83f35
Aaron Henderson <HENDERSONAW@ETSU.EDU>
2021-08-16 13:53:47 -0400

    Updated unit tests.

--- PasswordStrengthTester/app/tests.py
+++ PasswordStrengthTester/app/tests.py
@@ -5,6 +5,7 @@ when you run "manage.py test".
 
 import django
 from django.test import TestCase
+from .password import Password
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -23,3 +24,12 @@ class ViewTest(TestCase):
         response = self.client.get('/')
         self.assertContains(response, 'Home Page', 1, 200)
 
+    def test_banned_password(self):
+        """Tests the banned password list."""
+        word = Password("Password123")
+        self.assertLess(word.strengthScore, 20)
+
+    def test_balladhealth_password(self):
+        """Tests if password related to Ballad Health is not a Strong password."""
+        word = Password("BHealth2022!")
+        self.assertLess(word.strengthScore, 60)
